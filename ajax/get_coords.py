91396cc3d0b4a16d704a58f5a21d4a7feb33a5f7
jfk93-fr <85239242+jfk93-fr@users.noreply.github.com>
2023-02-15 16:47:13 +0100

    Update get_coords.py

--- ajax/get_coords.py
+++ ajax/get_coords.py
@@ -9,9 +9,7 @@ print('Content-Type: text-plain; charset=utf-8\r\n\r\n')
 
 dataurl = cgi.FieldStorage()
 locfile = dataurl.getvalue('analysis')
-#locfile = "analysis-20210217193701.json"
 fic = dir+'/'+locfile
-#print(fic)
 try:
     handle = open(fic, "r")
     infos = handle.read()
